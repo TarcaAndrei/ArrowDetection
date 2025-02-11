3e997353852b183a19bb454bbc30c4b91e14326e
TarcaAndrei <andrei.tarca100@gmail.com>
2025-02-11 11:37:41 +0100

    Added main model

--- ML/models/__init__.py
+++ ML/models/__init__.py
@@ -1,3 +1,5 @@
 from .detr_head import DETR
 from .transformer import Transformer
-from .backbone_resnet import BackboneResnet
+from .backbone_resnet import ResnetBackbone
+from .single_task_model import SingleTaskModel
+from .backbone_vit import BackboneViT
---
+++ ML/models/single_task_model.py
@@ -0,0 +1,34 @@
+import torch
+from torch import nn
+
+
+class SingleTaskModel(nn.Module):
+    """
+    An implementation of the :class:`nn.Module`.
+    """
+    backbone: nn.Module
+    head: nn.Module
+    def __init__(self, backbone: nn.Module, head: nn.Module, freeze_backbone: bool = True) -> None:
+        """
+        Args:
+            backbone: a :class:`torch.nn.Module` implementing the feature extractor backbone
+            head: a :class:`torch.nn.Module` implementing the head of the model
+            freeze_backbone: if set to True, the backbones parameters won't be updated
+        """
+        super().__init__()
+        self.backbone = backbone
+        if freeze_backbone:
+            for param in self.backbone.parameters():
+                param.requires_grad = False
+        self.head = head
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        """
+        forward function for backbone
+        Args:
+            inputs: the inputs of the model
+        Returns:
+            the output of the entire model
+        """
+        backbone_out = self.backbone(inputs)
+        return self.head(backbone_out)
